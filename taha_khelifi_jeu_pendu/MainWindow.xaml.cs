233d909d7d73fa31169ecf14280e14de40fba29b
taha khelifi <taha.khelifi@saintmichelannecy.fr>
2023-10-13 16:08:28 +0200

    1.8

--- taha_khelifi_jeu_pendu/MainWindow.xaml.cs
+++ taha_khelifi_jeu_pendu/MainWindow.xaml.cs
@@ -20,10 +20,8 @@ namespace taha_khelifi_jeu_pendu
     /// </summary>
     public partial class MainWindow : Window
     {
-        //variable
-
-        private List<string> motsEn4Lettres = new List<string> { "chat", "rare", "vent", "rond" };
-        private Random random = new Random();
+        private string motAdeviner;
+        private int viesRestantes = 4;
 
         public MainWindow()
         {
@@ -32,21 +30,77 @@ namespace taha_khelifi_jeu_pendu
 
         private void BTN_Commencer_Click_1(object sender, RoutedEventArgs e)
         {
+            // Réinitialisez le jeu.
+            viesRestantes = 4;
+            TB_Vie_Restant.Text = viesRestantes.ToString();
+
+            // Générez un nouveau mot aléatoire.
+            List<string> mots = new List<string> { "MOT1", "MOT2", "MOT3" /* Ajoutez plus de mots ici */ };
+            Random random = new Random();
+            motAdeviner = mots[random.Next(mots.Count)];
+
+            // Initialisez TB_Mot avec des astérisques.
+            TB_Mot.Text = new string('*', motAdeviner.Length);
+
+            // Réactivez les touches du clavier.
+            foreach (var child in ClavierGrid.Children.OfType<Button>())
             {
-                // Sélectionnez un mot aléatoire dans la liste
-                int indexMotAleatoire = random.Next(motsEn4Lettres.Count);
-                string motAleatoire = motsEn4Lettres[indexMotAleatoire];
-
-                // Créez une chaîne de caractères composée d'astérisques de la même longueur que le mot
-                string motCache = new string('*', motAleatoire.Length);
-
-                // Affichez le mot caché dans TextBlock TB_Mot
-                TB_Mot.Text = motCache;
+                if (child.Name != "BTN_Commencer")
+                {
+                    child.IsEnabled = true;
+                }
             }
         }
+
         private void Clavier_Click(object sender, RoutedEventArgs e)
         {
-            Button button = (Button)sender;
+            Button bouton = (Button)sender;
+            string lettre = bouton.Content.ToString();
+
+            if (motAdeviner.Contains(lettre))
+            {
+                // La lettre fait partie du mot à deviner.
+                char[] motActuel = TB_Mot.Text.ToCharArray();
+                for (int i = 0; i < motAdeviner.Length; i++)
+                {
+                    if (motAdeviner[i] == lettre[0])
+                    {
+                        motActuel[i] = lettre[0];
+                    }
+                }
+                TB_Mot.Text = new string(motActuel);
+                bouton.IsEnabled = false;
+            }
+            else
+            {
+                // La lettre ne fait pas partie du mot, réduisez les vies.
+                viesRestantes--;
+                TB_Vie_Restant.Text = viesRestantes.ToString();
+                bouton.IsEnabled = false;
+            }
+
+            if (viesRestantes == 0)
+            {
+                MessageBox.Show("Vous avez perdu !");
+                DesactiverTouchesClavier();
+            }
+            else if (!TB_Mot.Text.Contains("*"))
+            {
+                MessageBox.Show("Vous avez gagné !");
+                DesactiverTouchesClavier();
+            }
+        }
+
+        private void DesactiverTouchesClavier()
+        {
+            // Désactivez toutes les touches du clavier.
+            foreach (var child in ClavierGrid.Children.OfType<Button>())
+            {
+                if (child.Name != "BTN_Commencer")
+                {
+                    child.IsEnabled = false;
+                }
+            }
         }
     }
 }
