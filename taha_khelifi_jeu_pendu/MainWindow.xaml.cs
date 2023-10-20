b6f0efdf674cb4567605231402ae29799298d4ac
taha khelifi <taha.khelifi@saintmichelannecy.fr>
2023-10-20 09:02:10 +0200

    1.1

--- taha_khelifi_jeu_pendu/MainWindow.xaml.cs
+++ taha_khelifi_jeu_pendu/MainWindow.xaml.cs
@@ -18,31 +18,20 @@ namespace taha_khelifi_jeu_pendu
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
-        private string motAdeviner;
-        private int viesRestantes = 4;
+        private JeuPendu pendu;
 
         public MainWindow()
         {
             InitializeComponent();
+            pendu = new JeuPendu(TB_Vie_Restant, TB_Mot);
         }
 
         private void BTN_Commencer_Click_1(object sender, RoutedEventArgs e)
         {
-            // Réinitialisez le jeu.
-            viesRestantes = 4;
-            TB_Vie_Restant.Text = viesRestantes.ToString();
-
-            // Générez un nouveau mot aléatoire.
-            List<string> mots = new List<string> { "RARE", "JEUX", "BALLE" /* Ajoutez plus de mots ici */ };
-            Random random = new Random();
-            motAdeviner = mots[random.Next(mots.Count)];
-
-            // Initialisez TB_Mot avec des astérisques.
-            TB_Mot.Text = new string('*', motAdeviner.Length);
-
-            // Réactivez les touches du clavier.
+            pendu.InitialiserJeu();
             foreach (var child in ClavierGrid.Children.OfType<Button>())
             {
                 if (child.Name != "BTN_Commencer")
@@ -57,9 +46,61 @@ namespace taha_khelifi_jeu_pendu
             Button bouton = (Button)sender;
             string lettre = bouton.Content.ToString();
 
+            pendu.JouerTour(lettre);
+
+            if (pendu.EstPartiePerdue())
+            {
+                MessageBox.Show("Vous avez perdu !");
+                DesactiverTouchesClavier();
+            }
+            else if (pendu.EstPartieGagnee())
+            {
+                MessageBox.Show("Vous avez gagné !");
+                DesactiverTouchesClavier();
+            }
+        }
+
+        private void DesactiverTouchesClavier()
+        {
+            foreach (var child in ClavierGrid.Children.OfType<Button>())
+            {
+                if (child.Name != "BTN_Commencer")
+                {
+                    child.IsEnabled = false;
+                }
+            }
+        }
+    }
+
+    public class JeuPendu
+    {
+        private string motAdeviner;
+        private int viesRestantes = 4;
+        private TextBox TB_Vie_Restant;
+        private TextBox TB_Mot;
+
+        public JeuPendu(TextBox tbVieRestant, TextBox tbMot)
+        {
+            TB_Vie_Restant = tbVieRestant;
+            TB_Mot = tbMot;
+        }
+
+        public void InitialiserJeu()
+        {
+            viesRestantes = 4;
+            TB_Vie_Restant.Text = viesRestantes.ToString();
+
+            List<string> mots = new List<string> { "RARE", "JEUX", "BALLE" /* Ajoutez plus de mots ici */ };
+            Random random = new Random();
+            motAdeviner = mots[random.Next(mots.Count)];
+
+            TB_Mot.Text = new string('*', motAdeviner.Length);
+        }
+
+        public void JouerTour(string lettre)
+        {
             if (motAdeviner.Contains(lettre))
             {
-                // La lettre fait partie du mot à deviner.
                 char[] motActuel = TB_Mot.Text.ToCharArray();
                 for (int i = 0; i < motAdeviner.Length; i++)
                 {
@@ -69,38 +110,22 @@ namespace taha_khelifi_jeu_pendu
                     }
                 }
                 TB_Mot.Text = new string(motActuel);
-                bouton.IsEnabled = false;
             }
             else
             {
-                // La lettre ne fait pas partie du mot, réduisez les vies.
                 viesRestantes--;
                 TB_Vie_Restant.Text = viesRestantes.ToString();
-                bouton.IsEnabled = false;
-            }
-
-            if (viesRestantes == 0)
-            {
-                MessageBox.Show("Vous avez perdu !");
-                DesactiverTouchesClavier();
-            }
-            else if (!TB_Mot.Text.Contains("*"))
-            {
-                MessageBox.Show("Vous avez gagné !");
-                DesactiverTouchesClavier();
             }
         }
 
-        private void DesactiverTouchesClavier()
+        public bool EstPartiePerdue()
         {
-            // Désactivez toutes les touches du clavier.
-            foreach (var child in ClavierGrid.Children.OfType<Button>())
-            {
-                if (child.Name != "BTN_Commencer")
-                {
-                    child.IsEnabled = false;
-                }
-            }
+            return viesRestantes == 0;
+        }
+
+        public bool EstPartieGagnee()
+        {
+            return !TB_Mot.Text.Contains("*");
         }
     }
 }
